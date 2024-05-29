0bf2f8e7289300baffe85528667ee1cfdf5dac1f
Emmanuel <emmanuelbask@gmail.com>
2024-05-29 08:36:52 +0100

    All changes made

--- frontend/dahcoops/src/app.jsx
+++ frontend/dahcoops/src/app.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+// import preactLogo from './assets/preact.svg'
+// import viteLogo from '/vite.svg'
 import './app.css'
+import { Route, Routes } from 'react-router-dom'
+import { Account } from './pages/account'
+import { Home } from './pages/home'
+import { Transactions } from './pages/transactions'
+import { Finance } from './pages/finance'
+import { Profile } from './pages/profile'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <Routes>
+        <Route exact path="/" element={<Account/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/transactions" element={<Transactions/>} />
+        <Route path="/finance" element={<Finance/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
     </>
   )
 }
--- frontend/dahcoops/src/main.jsx
+++ frontend/dahcoops/src/main.jsx
@@ -1,5 +1,13 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
 import './index.css'
+import  { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme.js'
 
-render(<App />, document.getElementById('app'))
+
+render(<BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>, document.getElementById('app'))
